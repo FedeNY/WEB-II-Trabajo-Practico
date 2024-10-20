556108fda0bb8a3f5a619c7e3078cf467f8a5a58
FedeNY <fede1782015@hotmail.com>
2024-10-20 17:31:24 -0300

    Ultimas fases del proyecto AutoDeploy funcionando con cuenta Admin

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
     const MYSQL_USER = 'root';
     const MYSQL_PASS = '';
-    const MYSQL_DB = 'g160_db_prueba_tienda';
+    const MYSQL_DB = 'g160_db_tienda_celulares';
     const MYSQL_HOST = 'localhost';
